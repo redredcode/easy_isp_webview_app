0b8bf0c1ca849e7e7db4bf9930d47fc76d34e079
fahim <thebigredbhuiyan@gmail.com>
2026-02-21 03:30:02 +0600

    feat: add back functionality

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -19,6 +19,12 @@ class _WebviewScreenState extends State<WebviewScreen> {
       appBar: AppBar(
         title: Text('Easy ISP 24'),
         backgroundColor: Colors.deepPurple.shade50,
+        leading: IconButton(
+          onPressed: () {
+            controller.goBack();
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
       ),
       body: WebViewWidget(controller: controller),
     );
