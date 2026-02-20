28fe5a0af9f03c61d43cbbdf6c02d1add3c1e387
fahim <thebigredbhuiyan@gmail.com>
2026-02-21 03:36:10 +0600

    feat: phone back button navigation

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -15,18 +15,26 @@ class _WebviewScreenState extends State<WebviewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Easy ISP 24'),
-        backgroundColor: Colors.deepPurple.shade50,
-        leading: IconButton(
-          onPressed: () {
-            controller.goBack();
-          },
-          icon: Icon(Icons.arrow_back_ios),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (await controller.canGoBack()) {
+          controller.goBack();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Easy ISP 24'),
+          backgroundColor: Colors.deepPurple.shade50,
+          leading: IconButton(
+            onPressed: () {
+              controller.goBack();
+            },
+            icon: Icon(Icons.arrow_back_ios),
+          ),
         ),
+        body: WebViewWidget(controller: controller),
       ),
-      body: WebViewWidget(controller: controller),
     );
   }
 }
